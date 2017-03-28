76110a8650d3132b3d1d0a031629aadbcfa3e48c
beeva <israel_arroyo@live.com>
2017-03-28 11:27:29 -0600

    agregar readme

--- send_answers.py
+++ send_answers.py
@@ -7,19 +7,20 @@ import simplejson as json
 
 
 archivo = sys.argv[1]
-ip = sys.argv[2]
-puerto = sys.argv[3]
-idelastic = sys.argv[4]
+#ip = sys.argv[2]
+#puerto = sys.argv[3]
+#idelastic = sys.argv[4]
 
-url = 'http://localhost:9200/israel/israel/1"
+url = 'http://10.110.70.41:9200/graduates/quiz1/?pretty'
 
 
 
 json_data=open(archivo)
 data = json.load(json_data)
-json_data.close():
+json_data.close()
 
 #print "Dimension: ", data['cubes'][cube]['dim']
 #print "Measures:  ", data['cubes'][cube]['meas']
 headers = {'Content-type': 'application/json'}
 r = requests.post(url, data=json.dumps(data), headers=headers)
+print(r.text)
