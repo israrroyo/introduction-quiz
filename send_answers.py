7fc0e3c13f5b143144290a83523c7582eceb187a
beeva <israel_arroyo@live.com>
2017-03-28 11:07:03 -0600

    agregar readme

--- send_answers.py
+++ send_answers.py
@@ -9,14 +9,15 @@ import simplejson as json
 archivo = sys.argv[1]
 ip = sys.argv[2]
 puerto = sys.argv[3]
+idelastic = sys.argv[4]
 
-url = 'http://' + ip + ':' + puerto
+url = 'http://localhost:9200/israel/israel/1"
 
 
 
 json_data=open(archivo)
 data = json.load(json_data)
-json_data.close()
+json_data.close():
 
 #print "Dimension: ", data['cubes'][cube]['dim']
 #print "Measures:  ", data['cubes'][cube]['meas']
