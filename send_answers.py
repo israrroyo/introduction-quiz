82f90e2db2dacfaf64b9acfd5f37309b255b0fb6
beeva <israel_arroyo@live.com>
2017-03-28 11:31:30 -0600

    agregar readme

--- send_answers.py
+++ send_answers.py
@@ -19,8 +19,7 @@ json_data=open(archivo)
 data = json.load(json_data)
 json_data.close()
 
-#print "Dimension: ", data['cubes'][cube]['dim']
-#print "Measures:  ", data['cubes'][cube]['meas']
+
 headers = {'Content-type': 'application/json'}
 r = requests.post(url, data=json.dumps(data), headers=headers)
 print(r.text)
